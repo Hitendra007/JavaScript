cf28c924bb4462232d625130bbd6498e2e31e2ed
hitendra <hitendra369432@gmail.com>
2023-11-03 10:43:26 +0530

    numbers

---
+++ 07_nums_and_math.js
@@ -0,0 +1,27 @@
+const score = 400
+// console.log(typeof (score))
+const balance = new Number(199)
+// console.log(balance)
+// console.log(balance.toString(), typeof (balance.toString()))
+// console.log(balance.toFixed(2))
+const othrnum = 23.554
+// console.log(othrnum.toPrecision(2))
+
+const hundereds = 1000000
+
+// console.log(hundereds.toLocaleString('en-IN'));
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+
+// ++++++++++++++++++++++++++ Maths +++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-3))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+
+// console.log((Math.random()*10)+1)
+min = 10
+max = 20
+console.log(Math.floor((Math.random() * (max - min + 1))+ min))
+
